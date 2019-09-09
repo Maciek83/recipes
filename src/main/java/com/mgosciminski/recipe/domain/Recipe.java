31ee7291e88f12864e03fb8a9058ee641a28d6c0
MaciejGosciminski <zimneognie@gmail.com>
2019-09-09 06:58:13 +0200

    add entity and relations beetween racipe and note closes #7

--- src/main/java/com/mgosciminski/recipe/domain/Recipe.java
+++ src/main/java/com/mgosciminski/recipe/domain/Recipe.java
@@ -1,13 +1,27 @@
 package com.mgosciminski.recipe.domain;
 
+import javax.persistence.*;
+
+@Entity
 public class Recipe {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String description;
     private Integer prepTime;
     private Integer cookTime;
     private Integer servings;
     private String source;
     private String url;
+    @Lob
     private String directions;
+    @Enumerated(EnumType.STRING)
     private Difficulty difficulty;
+
+    @OneToOne(cascade = CascadeType.ALL,mappedBy = "recipe")
+    private Note notes;
+
+
+
 }
