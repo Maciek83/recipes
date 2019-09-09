8d3bffd08e92938ff2d563f16729f6a4806b92e6
MaciejGosciminski <zimneognie@gmail.com>
2019-09-09 05:47:42 +0200

    add recipe domain closes #1

---
+++ src/main/java/com/mgosciminski/recipe/domain/Recipe.java
@@ -0,0 +1,13 @@
+package com.mgosciminski.recipe.domain;
+
+public class Recipe {
+
+    private String description;
+    private Integer prepTime;
+    private Integer cookTime;
+    private Integer servings;
+    private String source;
+    private String url;
+    private String directions;
+    // todo add difficulty enum
+}
